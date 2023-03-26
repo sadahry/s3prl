0fe1c1dea9e2a52e3a4e5156dbf06dc30fcdb4b4
sadahry <sadahiroyoshi@gmail.com>
2023-03-26 09:16:33 +0000

    rm unused

--- s3prl/upstream/data2vec2_spectrogram/hubconf.py
+++ s3prl/upstream/data2vec2_spectrogram/hubconf.py
@@ -1,7 +1,3 @@
-import os
-
-import torch
-
 from s3prl.util.download import _urls_to_filepaths
 
 from .expert import UpstreamExpert as _UpstreamExpert
@@ -16,37 +12,3 @@ def data2vec2_spectrogram_custom(ckpt: str, refresh: bool = False, **kwargs):
 
 def data2vec2_spectrogram_local(*args, **kwargs):
     return data2vec2_spectrogram_custom(*args, **kwargs)
-
-
-def data2vec2_spectrogram_url(*args, **kwargs):
-    return data2vec2_spectrogram_custom(*args, **kwargs)
-
-
-def data2vec2_spectrogram(refresh=False, *args, **kwargs):
-    """
-    The default model - Base
-        refresh (bool): whether to download ckpt/config again if existed
-    """
-    return data2vec2_spectrogram_base_960(refresh=refresh, *args, **kwargs)
-
-
-def data2vec2_spectrogram_base_960(refresh=False, *args, **kwargs):
-    """
-    The Base model
-        refresh (bool): whether to download ckpt/config again if existed
-    """
-    kwargs[
-        "ckpt"
-    ] = "https://huggingface.co/s3prl/converted_ckpts/resolve/main/audio_base_ls.pt"
-    return data2vec2_spectrogram_custom(refresh=refresh, *args, **kwargs)
-
-
-def data2vec2_spectrogram_large_ll60k(refresh=False, *args, **kwargs):
-    """
-    The Large model trained on Libri-light 60k hours of data
-        refresh (bool): whether to download ckpt/config again if existed
-    """
-    kwargs[
-        "ckpt"
-    ] = "https://huggingface.co/s3prl/converted_ckpts/resolve/main/vox_pretrained.pt"
-    return data2vec2_spectrogram_custom(refresh=refresh, *args, **kwargs)
